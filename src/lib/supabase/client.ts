fc07432a7f4c0b3fa9f300cf5675244d817a78b4
Forelolit <aktantai2005@gmail.com>
2026-03-07 14:01:28 +0600

    fix

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,12 @@
 import { Database } from '@/types/database.types';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+export const getSupabaseClient = () => {
+    if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+        throw new Error('Supabase URL and ANON_KEY must be set');
+    }
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+    return createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+};
+
+export const supabase = getSupabaseClient();
