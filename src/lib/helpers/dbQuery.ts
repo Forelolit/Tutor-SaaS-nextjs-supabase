2687e1138990f476a6feb00cc1bc0a99d07853af
Forelolit <aktantai2005@gmail.com>
2026-03-14 07:23:24 +0600

    fix

--- src/lib/helpers/dbQuery.ts
+++ src/lib/helpers/dbQuery.ts
@@ -1,7 +1,13 @@
-export const dbQuery = async (promise) => {
+export const dbQuery = async <T>(promise: Promise<{ data: T | null; error: unknown }>): Promise<T> => {
     const { data, error } = await promise;
 
-    if (error) throw error;
+    if (error) {
+        throw error;
+    }
+
+    if (data === null) {
+        throw new Error('No data returned from Supabase');
+    }
 
     return data;
 };
